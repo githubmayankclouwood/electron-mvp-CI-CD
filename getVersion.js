e61359f3bf34df5e458e5190df385b45be8c1c53
Clouwood <mayank@clouwood.com>
2024-10-25 16:23:25 +0530

    Test github action-3

--- getVersion.js
+++ getVersion.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import fs from 'fs'
 
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-console.log(`::set-output name=version::${packageJson.version}`);
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+console.log(`::set-output name=version::${packageJson.version}`)
